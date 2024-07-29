fa0254a642e9ff55c134c8b1c159b58a739208c1
Marc Christian D.  Tumaneng <145457643+mirkorio@users.noreply.github.com>
2024-07-29 10:43:35 +0800

    Delete Method Overloading (Programs)-20221218 directory

--- Method Overloading (Programs)-20221218/OverloadRunnable.java
+++
@@ -1,9 +0,0 @@
-public class OverloadRunnable{
-	public static void main(String[] args) {
-		Overload.add(12, 13);
-		Overload.add(67.2, 23.5);
-		Overload.add(1, 2, 3);
-		Overload.add(23.4, 10);
-		Overload.add("Hello", "World");
-	}
-}
